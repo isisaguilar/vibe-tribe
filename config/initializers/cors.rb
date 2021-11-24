1a7d319060f9f2ba330287a4464653863e6a0888
isis <icey1727@gmail.com>
2021-11-24 14:37:59 -0500

    configures frontend domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080','viibe-triibe.herokuapp.com'
+    origins 'localhost:8080','vibe-tribe.netlify.app/'
 
     resource '*',
       headers: :any,
